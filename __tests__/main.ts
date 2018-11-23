f4000ce8b25d9cb8242afd7bd84e62878edc29bb
Rahman Fadhil <rhmnfadhil@gmail.com>
2018-11-23 22:09:05 +0700

    Switch test to createStore

--- __tests__/main.ts
+++ __tests__/main.ts
@@ -3,7 +3,7 @@ import { createStore, Statefully } from "../src";
 describe("Main", () => {
   test("Should passed basic test", async () => {
     const initialState = { hello: "world" };
-    const store = new Statefully(initialState);
+    const store = createStore(initialState);
     expect(store instanceof Statefully).toBeTruthy();
 
     store.register("setHello", () => ({ hello: "changed" }));
