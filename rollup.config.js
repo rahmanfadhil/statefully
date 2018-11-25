969bcb12a088fd8df6dfcb2c40e0338df4b97014
Rahman Fadhil <rhmnfadhil@gmail.com>
2018-11-25 09:48:41 +0700

    0.1.4

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,5 @@
 /* eslint-disable */
 import typescript from "rollup-plugin-typescript2";
-import { uglify } from "rollup-plugin-uglify";
 
 export default {
   input: "./src/index.ts",
@@ -9,12 +8,16 @@ export default {
       clean: true,
       tsconfigOverride: { exclude: ["**/*.spec.ts"] },
     }),
-    uglify(),
   ],
   output: [
     {
       file: "dist/index.js",
       format: "cjs",
+    },
+    {
+      file: "dist/index.min.js",
+      name: "Statefully",
+      format: "umd",
     },
   ],
 };
